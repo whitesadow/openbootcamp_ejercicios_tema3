f40c1abe6db035846fe14d5d82e83e94b0f237a4
whitesadow <101958892+whitesadow@users.noreply.github.com>
2022-11-01 19:10:44 -0500

    Delete Micoche.java

--- Micoche.java
+++
@@ -1,6 +0,0 @@
-public class Micoche {
-    public static void main(String[] args){
-        String marca = "Ferrari", modelo = "SF90 Stralade";
-        Coche Micoche = new Coche(marca, modelo, "rojo", 17, 01, 2021);
-    }
-}
